b23e2ec71816db411fc63aff713d84acac9ded50
bryangholipour <113397778+bryangholipour@users.noreply.github.com>
2022-11-08 10:34:12 -0800

    seed data

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const sequelize = require('../config/connection');
+const seedUsers = require('./'); //ADD FILE PATHS
+const seedBirds = require('./birdData');
+const seedEvents = require('./') //ADD FILE PATHS
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedBirds();
+
+  await seedEvents();
+
+  await seedUsers();
+
+  process.exit(0);
+};
+
+seedAll();
