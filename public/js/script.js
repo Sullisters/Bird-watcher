088dd158aa0f8a38986a775d998e9fcac443b61e
bryangholipour <113397778+bryangholipour@users.noreply.github.com>
2022-11-10 11:31:34 -0800

    cloudinary

--- public/js/script.js
+++ public/js/script.js
@@ -27,6 +27,29 @@ function gettingInput(event) {
 //  ALTERNATIVE GIVEN BY THE API SITE
 console.log("hello")
 
+var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'my-cloud', 
+    uploadPreset: 'preset1'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+      }
+    }
+  )
+
+document.getElementById("upload_widget").addEventListener("click", function(event){
+    event.preventDefault();
+    myWidget.open();
+  }, false);
+
+  cloudinary.openUploadWidget({
+      cloudName: "my-cloud", uploadPreset: "preset1" }, (error, result) => { });
+
+  cloudinary.applyUploadWidget(document.getElementById('upload_widget'), 
+{ api_key : "289214219512654", cloudName: "my-cloud", uploadSignature: generateSignature }, 
+(error, result) => { });
+
+// widget.open();
+
 searchBtn.addEventListener('click', startSearch);
 
 function startSearch(event) {
@@ -54,4 +77,8 @@ function startSearch(event) {
         alert('unable to connect to the data' + error)
     })
 }
+
+
+  
+
 
