30e9778e89dae021cfcb111fc7d16d8e13246585
bryangholipour <113397778+bryangholipour@users.noreply.github.com>
2022-11-11 14:29:03 -0800

    cloudinary image URL save

--- public/js/Cloudinary.js
+++ public/js/Cloudinary.js
@@ -1,67 +1,76 @@
-// require('dotenv').config();
-// const cloudinary = require('cloudinary').v2;
-const cloudName = 'df4asanhm';
-const uploadPreset = 'w7sz2h8r';
+// require("dotenv").config()
+// const express = require("express")
+// const cloudinary = require("cloudinary").v2
+// const app = express()
+const cloudName = "df4asanhm";
+const uploadPreset = "w7sz2h8r";
 
-// const formData = new FormData()
-// formData.append('file', base64Image)
-// formData.append('upload_preset', 'w7sz2h8r')
+const formData = new FormData();
+formData.append("cloud_name", "df4asanhm");
+formData.append("upload_preset", "w7sz2h8r");
 
-// fetch(`https://api.cloudinary.com/v1_1/***YOUR_CLOUD_NAME***/image/upload`, {
-//   method: 'POST',
-//   body: formData,
-// })
-
-document.getElementById("upload_widget").addEventListener("click", function(event){
-    event.preventDefault();
-    myWidget.open();
-  }, false);
-
-var myWidget = cloudinary.createUploadWidget({
-    cloudName: 'df4asanhm', 
-    uploadPreset: 'w7sz2h8r',
-    // multiple: false,
-    // folder: "user_images",
-    // maxImageFileSize: 2000000,
-    // maxImageWidth: 2000,
+var myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: "df4asanhm",
+    uploadPreset: "w7sz2h8r",
     theme: "purple",
     cropping: true,
-    // showSkipCropButton: true,
-  }, 
-    (error, result) => { 
-      if (!error && result && result.event === "success") { 
-        console.log('Done! Here is the image info: ', result.info); 
-        document
-        .getElementById("uploadedimage")
-        .setAttribute("src", result.info.secure_url);
-      }
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log("Done! Here is the public ID: ", result.info.public_id);
+      document.getElementById("uploadedimage");
+      var publicId = result.info.public_id;
+      var imageUrl =
+        "https://res.cloudinary.com/df4asanhm/image/upload/" +
+        publicId +
+        ".jpg";
+      console.log(imageUrl);
+      // .setAttribute("src", result.info.secure_url);
     }
-  )
+  }
+);
+// fetch(`https://api.cloudinary.com/v1_1/df4asanhm/image/upload`, {
+//   method: "POST",
+//   body: formData,
+// });
 
-//   cloudinary.openUploadWidget({
-//       cloudName: "my-cloud", uploadPreset: "preset1" }, (error, result) => { });
+document.getElementById("upload_widget").addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    myWidget.open();
+  },
+  false
+);
 
-  cloudinary.applyUploadWidget(document.getElementById('upload_widget'), 
-{ api_key : "289214219512654", cloudName: "df4asanhm", uploadPreset: 'w7sz2h8r'}, 
-(error, result) => { });
+// cloudinary.openUploadWidget({
+//     cloudName: "my-cloud", uploadPreset: "preset1" }, (error, result) => { });
 
-widget.open();
+//   cloudinary.applyUploadWidget(document.getElementById('upload_widget'),
+// { api_key : "289214219512654", cloudName: "df4asanhm", uploadPreset: 'w7sz2h8r'},
+// (error, result) => { });
 
-var data = new FormData();
-data.append('upload_preset', 'w7sz2h8r');
-data.append('file', file);
-data.append('cloud_name', 'df4asanhm');
+// var data = new FormData();
+// data.append('upload_preset', 'w7sz2h8r');
+// data.append('cloud_name', 'df4asanhm');
 
-const config = {
-    method: "POST",
-    body: data 
-};
+// const config = {
+//     method: "POST",
+//     body: data
+// };
 
-var imgurl = "https://api.cloudinary.com/v1_1/<cloud_name>/image/upload";
-
-fetch(imgurl, config)
-.then(responseData => {
-          console.log(JSON.stringify(responseData, null, 4));
-})
+// var imgurl = "https://api.cloudinary.com/v1_1/df4asanhm/image/upload";
+// fetch(imgurl, config)
+// .then(responseData => {
+//           console.log(JSON.stringify(responseData, null, 4));
+// })
+// cloudinary.api.create_upload_preset({
+//     name: 'demo_preset',
+//     folder: 'main',
+//     allowed_formats: 'jpg, png'
+// })
+// .then(uploadResult => console.log(uploadResult))
+// .catch(error => console.error(error));
 
 
--- public/js/upload-preset.js
+++
@@ -1,7 +0,0 @@
-cloudinary.api.create_upload_preset({
-    name: 'demo_preset',
-    folder: 'main',
-    allowed_formats: 'jpg, png'
-})
-.then(uploadResult => console.log(uploadResult))
-.catch(error => console.error(error));
