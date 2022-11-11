2bd998d24dc370061769cf95ec3d325daede6b67
D-Dursty <devondahlia@gmail.com>
2022-11-11 10:38:58 -0800

    stuff

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -9,17 +9,47 @@ const axios = require('axios');
 router.get("/",(req,res)=>{
     res.render("home")
 })
+router.get("/login",(req,res)=>{
+    if (req.session.logged_in){
+        return res.redirect(`/events`);
+    }
+    res.render("login", {
+        logged_in: req.session.logged_in
+    })
+})
+
+// router.get('/signup',(req,res)=>{
+//     if(req.session.loggedIn){
+//         return res.redirect(`/user/${req.session.userId}`)
+//     }
+//     res.render("signup",{
+//         loggedIn:false,
+//         userId:null
+//     })
+// })
+
+router.get("/newAccount", (req, res) => {
+    console.log(req.session.logged_in);
+    if (req.session.logged_in) {
+     return res.redirect('/events');
+    } 
+    res.render('newAccount');
+})
+
 
 router.get("/events",(req,res)=>{
+    if (!req.session.logged_in){
+        return res.redirect("/login")
+    }
    Event.findAll().then(events=>{
     const eventsHbsData = events.map(event=>event.get({plain:true}))
-    res.render("event", {
+    res.render("journal", {
         events:eventsHbsData
         })
     })
 })
 
-router.get("/event/:id",(req,res)=>{
+router.get("/journal/:id",(req,res)=>{
    Event.findByPk(req.params.id,{
     include: [User]
    }).then(event=>{
@@ -31,19 +61,30 @@ router.get("/event/:id",(req,res)=>{
     // res.render("bird-api-details")
 })
 
+router.get("/bird-details/:id"), (req,res)=>{
+    Bird.findByPk(req.params.id,{
+        include: [sighting]
+    }).then(bird =>{
+        const birdHbsData = bird.map(bird=>bird.get({plain:true}));
+        console.log(bird);
+        console.log(birdHbsData);
+        res.render("journal", birdHbsData);
+    })
+}
+
 router.get("/sighting/:id",(req,res)=>{
     Sighting.findByPk(req.params.id,{
         include: [Bird]
     }).then(sighting=>{
-        const sightingHbsData = sighting.get({plain:true});
+        const sightingHbsData = sighting.map(sighting => sighting.get({plain:true}));
         console.log(sighting);
         console.log(sightingHbsData);
         res.render("journal", sightingHbsData)
     })
 })
 
-router.get("/login",(req,res)=>{
-    res.render("login")
-})
+router.get("/sessions", (req, res) => {
+    res.json(req.session);
+  });
 
 module.exports = router;
