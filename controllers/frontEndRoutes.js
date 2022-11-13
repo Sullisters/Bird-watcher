e46eea22fc2589ab66c342fe50db37d1c994e812
D-Dursty <devondahlia@gmail.com>
2022-11-13 08:26:26 -0800

    routes and stuff

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -21,7 +21,7 @@ router.get("/login",(req,res)=>{
 });
 
 router.get("/users/:id", (req, res) => {
-    if (!req.session.loggedIn) {
+    if (!req.session.logged_in) {
       return res.redirect(`/login`);
     }
     User.findByPk(req.params.id)
@@ -47,7 +47,7 @@ router.get("/newAccount", (req, res) => {
 })
 
 router.get("/events",(req,res)=>{
-    if (!req.session.loggedIn){
+    if (!req.session.logged_in){
         return res.redirect("/login")
     }
    Event.findAll().then(events=>{
@@ -58,52 +58,51 @@ router.get("/events",(req,res)=>{
     })
 })
 
-// router.get("/journal/:id", (req,res)=>{
+router.get("/journal/:id", (req,res)=>{
+    Event.findByPk(req.params.id,{
+        include:[Bird]
+    });
+    const newBird = Bird.findAll().then(birds =>{
+        const birdHbsData = birds.map(birds=>birds.get({plain:true}))
+        res.render("journal", {birds:birdHbsData})
+    })
+})
+
+// router.get("/journal/:id",(req,res)=>{
 //     Event.findByPk(req.params.id,{
-//         include:[Bird]
-//     });
-//     const newBird = Bird.findAll().then(birds =>{
-//         const birdHbsData = birds.map(birds=>birds.get({plain:true}))
-//         res.render("journal", {birds:birdHbsData})
-//     })
-// })
-
-router.get("/journal/:id",(req,res)=>{
-   Event.findByPk(req.params.id,{
-    include: [Bird]
-   }).then(newBird=>{
-    const birdHbsData = newBird.map(newBird => newBird.get({plain:true}));
-    console.log("we are in communicado");
-    console.log(birdHbsData);
-    res.render("journal",{
-        newbird:birdHbsData});
-    })
-})
-
-router.get("/bird-details/:id"), (req,res)=>{
-    Bird.findByPk(req.params.id,{
-        include: [sighting]
-    }).then(bird =>{
-        const birdHbsData = bird.map(bird=>bird.get({plain:true}));
-        console.log(bird);
-        console.log(birdHbsData);
-        res.render("journal", birdHbsData);
-    })
-}
-
-router.get("/sighting/:id",(req,res)=>{
-    Sighting.findByPk(req.params.id,{
-        include: [Bird]
-    }).then(sighting=>{
-        const sightingHbsData = sighting.map(sighting => sighting.get({plain:true}));
-        console.log(sighting);
-        console.log(sightingHbsData);
-        res.render("journal", sightingHbsData)
-    })
-})
-
-router.get("/sessions", (req, res) => {
-    res.json(req.session);
-  });
-
-module.exports = router;
+//      include: [Bird]
+//     }).then(bird=>{
+//      const birdHbsData = bird.get({plain:true});
+//      console.log("yayyyyy");
+//      console.log(birdHbsData);
+//      res.render("journal",birdHbsData);
+//      })
+//  })
+ 
+ router.get("/bird-details/:id"), (req,res)=>{
+     Bird.findByPk(req.params.id,{
+         include: [sighting]
+     }).then(bird =>{
+         const birdHbsData = bird.map(bird=>bird.get({plain:true}));
+         console.log(bird);
+         console.log(birdHbsData);
+         res.render("journal", birdHbsData);
+     })
+ }
+ 
+ router.get("/sighting/:id",(req,res)=>{
+     Sighting.findByPk(req.params.id,{
+         include: [Bird]
+     }).then(sighting=>{
+         const sightingHbsData = sighting.map(sighting => sighting.get({plain:true}));
+         console.log(sighting);
+         console.log(sightingHbsData);
+         res.render("journal", sightingHbsData)
+     })
+ })
+ 
+ router.get("/sessions", (req, res) => {
+     res.json(req.session);
+   });
+ 
+ module.exports = router;
