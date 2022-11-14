55214a219ab36a3c7590bd1535a021c8d4f8891f
Sullisters <heatonpschreiber@gmail.com>
2022-11-14 17:50:43 -0500

    WIP monday night bootstrap

--- public/js/cloudinary2.js
+++ public/js/cloudinary2.js
@@ -1,7 +1,47 @@
 const apiKey = `289214219512654`;
 const cloudName = `df4asanhm`;
 
-const pictureLink = "";
+let pictureLink = "";
+
+const newBird = document.querySelector('#birdInput');
+
+console.log(window.location.href.split("/")[4])
+
+newBird.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const birdObj = {
+        name: document.querySelector('#birdName').value,
+        bird_species: document.querySelector('#birdSpecies').value,
+        bird_colors: document.querySelector('#birdColors').value,
+        bird_size: document.querySelector('#birdSize').value,
+        weight: document.querySelector('#birdWeight').value,
+        bird_diet: document.querySelector('#birdDiet').value,
+        migration: document.querySelector('#birdMigration').value,
+        habitat: document.querySelector('#birdHabitat').value,
+        location: document.querySelector('#birdLocation').value,
+        activity: document.querySelector('#birdActivity').value,
+        description: document.querySelector('#birdNotes').value,
+        EventId: window.location.href.split("/")[4],
+        bird_image: pictureLink 
+        // add in a variable in here for the cloudinary link 
+    }
+    console.log(JSON.stringify(birdObj))
+    const res = await fetch("/api/birds/", {
+        method: "POST",
+        body: JSON.stringify(birdObj),
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    }) 
+    if (res.ok) {
+       console.log("yessss")
+       location.reload();
+    } else {
+      console.log(res)
+        alert("Try again!")
+    }
+})
 
 
 // cloudinary put request
@@ -12,15 +52,7 @@ const myWidget = cloudinary.createUploadWidget({
     uploadPreset: 'w7sz2h8r'}, (error, result) => { 
       if (!error && result && result.event === "success") { 
         console.log('WE PUT A BIRD ON IT', result.info.path); 
-        const body = {bird_image: result.info.path}
-        fetch(`/api/birds`,{
-            method:"POST",
-            body:JSON.stringify(body),
-            headers: {
-              "Content-Type": "application/json"
-            },
-            
-        })
+        pictureLink = result.info.path
         // const saveImageToDiv = fetch(body);
         // const letsDoThis = saveImageToDiv.json();
         // return letsDoThis
